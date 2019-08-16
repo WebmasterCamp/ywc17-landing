44394e1fa4fff517c678c6e154f406abfce03180
Anawin W <imonmac@hotmail.com>
2019-08-16 22:35:40 +0700

    Add ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    "@nuxtjs"
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+    "semi": [2, "never"],
+    "no-console": "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/html-self-closing": "off"
+  }
+}
