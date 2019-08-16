02b4526962f307de27665652a81e5319a7da89fc
Anawin W <imonmac@hotmail.com>
2019-08-16 23:56:09 +0700

    Add Google Fonts CSS

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,17 +13,18 @@ export default {
 
       { name: 'og:url', content: 'https://ywc17.ywc.in.th' },
       { name: 'og:type', content: 'website' },
-      { name: 'og:title', content: '17th Young Webmaster Camp'},
-      { name: 'og:description', content: ''},
-      { name: 'og:image', content: ''},
-      { name: 'article:author', content: 'https://www.facebook.com/ywcth'},
+      { name: 'og:title', content: '17th Young Webmaster Camp' },
+      { name: 'og:description', content: '' },
+      { name: 'og:image', content: '' },
+      { name: 'article:author', content: 'https://www.facebook.com/ywcth' },
 
       { name: 'twitter:card', content: 'summary_large_image' },
       { name: 'twitter:image:src', content: '' }
     ],
     link: [
       { rel: 'icon', type: 'image/png', href: '/favicon.png' },
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat:400,700|Sarabun&display=swap&subset=thai' }
     ]
   },
   /*
