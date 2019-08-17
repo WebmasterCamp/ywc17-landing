149fb36d7806059831835d74a71f344d1f060286
Anawin W <imonmac@hotmail.com>
2019-08-18 03:44:18 +0700

    Fix typo in config

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,6 @@ module.exports = {
     "no-trailing-spaces": "off",
     "vue/max-attributes-per-line": "off",
     "vue/html-self-closing": "off",
-    "vue/singleline-html-element-content-newline": off
+    "vue/singleline-html-element-content-newline": "off"
   }
 }
