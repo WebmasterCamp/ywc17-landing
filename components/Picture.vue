96c4e23b65822d514a6de8d91697aa68a4fdaaca
bazsup <supawit.ruen@mail.kmutt.ac.th>
2019-08-18 17:26:34 +0700

    Add Hero section
    Signed-off-by: bazsup <supawit.ruen@mail.kmutt.ac.th>

---
+++ components/Picture.vue
@@ -0,0 +1,48 @@
+<template>
+  <picture>
+    <source :srcset="webp" type="image/webp">
+    <img :src="defaultImage" :alt="alt" />
+  </picture>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+interface GetImageParams {
+  type: string
+}
+
+export default Vue.extend({
+  props: {
+    fileName: {
+      default: '',
+      type: String
+    },
+    alt: {
+      default: '',
+      type: String
+    },
+    defaultType: {
+      default: 'png',
+      type: String
+    }
+  },
+  computed: {
+    defaultImage (): Function {
+      return this.getImage({ type: this.defaultType })
+    },
+    webp (): Function {
+      return this.getImage({ type: 'webp' })
+    }
+  },
+  methods: {
+    getImage ({ type }: GetImageParams): Function {
+      return require('~/assets/images/' + this.fileName + '.' + type)
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+
+</style>
