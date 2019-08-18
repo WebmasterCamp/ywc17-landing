0f3ced90aa26ebfc5b05c0fbcafbdbe08ab26115
Anawin W <imonmac@hotmail.com>
2019-08-18 22:41:08 +0700

    Add Timeline section

---
+++ components/sections/Timeline.vue
@@ -0,0 +1,100 @@
+<template>
+  <section>
+    <SectionHead title="Timeline" />
+    <TimelineList>
+      <TimelineItem v-for="(item, code) in items" :key="code">
+        <TimelineIcon :active="isActive(code)">
+          <img :src="require(`~/assets/images/timeline-${code}.png`)" />
+        </TimelineIcon>
+        <TimelineName>{{ item.name }}</TimelineName>
+        {{ item.endDate ? rangeDate(item.startDate, item.endDate) : humanDate(item.startDate) }}
+      </TimelineItem>
+    </TimelineList>
+  </section>
+</template>
+
+<script>
+import styled from 'vue-styled-components'
+import dayjs from 'dayjs'
+import 'dayjs/locale/th'
+import color from '~/utils/color'
+import SectionHead from '~/components/SectionHead.vue'
+
+dayjs.locale('th')
+
+const TimelineList = styled.div`
+  display: grid;
+  grid-column-gap: 25px; 
+  grid-template-columns: auto auto auto auto auto;
+`
+const TimelineItem = styled.div`
+  display: inline-grid;
+  font-size: 14px;
+`
+const TimelineIcon = styled('div', { code: String, active: Boolean })`
+  width: 128px;
+  height: 128px;
+  line-height: 128px;
+  margin: 0 auto;
+  background: url(${(props) => { return require('~/assets/images/timeline-item' + (props.active ? '-active' : '') + '.png') }}) no-repeat center;
+`
+const TimelineName = styled.h3`
+  font-family: 'Maledpan';
+  font-size: 20px;
+  color: ${color.main};
+  margin-bottom: 0;
+`
+export default {
+  components: {
+    SectionHead,
+    TimelineList,
+    TimelineItem,
+    TimelineIcon,
+    TimelineName
+  },
+  data () {
+    return {
+      items: {
+        register: { name: 'รับสมัคร', startDate: '2019-09-01', endDate: '2019-10-15' },
+        announce: { name: 'ประกาศผล', startDate: '2019-10-26' },
+        interview: { name: 'สัมภาษณ์', startDate: '2019-11-02' },
+        finalist: { name: 'ประกาศผลสัมภาษณ์', startDate: '2019-11-05' },
+        camp: { name: 'วันค่าย', startDate: '2019-12-26', endDate: '2019-12-29' }
+      }
+    }
+  },
+  methods: {
+    isActive (code) {
+      const today = dayjs(new Date())
+      const startDate = dayjs(this.items[code].startDate)
+      if (typeof this.items[code].endDate === 'undefined') {
+        return startDate.isSame(today)
+      } else {
+        const endDate = dayjs(this.items[code].endDate)
+
+        if (endDate.isBefore(today)) {
+          return false
+        } else if (!startDate.isBefore(today) && !startDate.isSame(today)) {
+          return false
+        }
+        return true
+      }
+    },
+    humanDate (dateStr) {
+      return dayjs(dateStr).format('D MMMM')
+    },
+    rangeDate (startDate, endDate) {
+      let str = this.humanDate(startDate)
+      if (endDate) {
+        str += ` - ${this.humanDate(endDate)}` 
+      }
+      return str
+    }
+  }
+}
+</script>
+<style scoped>
+img {
+  vertical-align: middle;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,21 @@
     <Hero />
     <WhatIsYWC />
     <Majors />
+    <Timeline />
   </div>
 </template>
 
 <script>
 import WhatIsYWC from '~/components/sections/WhatIsYWC.vue'
 import Majors from '~/components/sections/Majors.vue'
+import Timeline from '~/components/sections/Timeline.vue'
 
 export default {
   components: {
     Hero: () => import('~/components/sections/Hero'),
     WhatIsYWC,
-    Majors
+    Majors,
+    Timeline
   }
 }
 </script>
