e1a9c5921336b27ad1e352a1d5b4c5209124ba17
Anawin W <imonmac@hotmail.com>
2019-08-31 17:50:52 +0700

    🔧 Fix Location font and responsive

--- components/sections/Location.vue
+++ components/sections/Location.vue
@@ -1,21 +1,19 @@
 <template>
-  <section class="continaer">
-    <div class="location" :style="{height: '600px',backgroundColor: 'white',position: 'relative'}">
+  <section>
+    <div class="location" :style="{height: '600px',position: 'relative'}">
       <div class="icon-container">
-      <div class="icon">LO<br/>CA<br/>TI<br/>ON</div>
+        <div class="icon">LO<br/>CA<br/>TI<br/>ON</div>
       </div>
-<div class="location-content">
-  <div class="video">
-
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.793301226501!2d100.77898871535137!3d13.730960501474286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d66308ce98ffd%3A0xcb43a76f038c38ca!2sFaculty%20of%20Information%20Technology!5e0!3m2!1sen!2sth!4v1567244733954!5m2!1sen!2sth" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
- 
-  </div>
-  <div class="detail">
-    <span class="highlight">สถาบันเทคโนโลยี</span>
-    <br/>
-    พระจอมเกล้าเจ้าคุณทหารลาดกระบัง
-  </div>
-</div>
+      <div class="location-content">
+        <div class="video">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.793301226501!2d100.77898871535137!3d13.730960501474286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d66308ce98ffd%3A0xcb43a76f038c38ca!2sFaculty%20of%20Information%20Technology!5e0!3m2!1sen!2sth!4v1567244733954!5m2!1sen!2sth" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        </div>      
+      </div>
+    </div>
+    <div class="detail">
+      <span class="highlight">สถาบันเทคโนโลยี</span>
+      <br/>
+      พระจอมเกล้าเจ้าคุณทหารลาดกระบัง
     </div>
   </section>
 </template>
@@ -31,11 +29,14 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-.location {
+section {
   margin:5rem 0;
   background-image: url(~assets/images/location.png);
   background-repeat: no-repeat;
   background-size :100% 100%;
+  padding-bottom: 5px;
+}
+.location {
   
     display: grid;
   @media screen and (min-width: 768px) {
@@ -56,22 +57,24 @@ export default Vue.extend({
   display: flex;
   flex-direction: column;
   justify-content: center;
-  font-family: Montserrat;
+  font-family: 'Maledpan';
+  font-weight: bold;
+  line-height: 0.5;
   text-align: right;
-  font-size: 2.5rem;
+  font-size: 48px;
   margin: 3rem 0;
+  padding-right: 4rem;
   @media(max-width:768px) {
     font-size: 22px;
     line-height: 1em;
+    margin: 1rem 0;
+    padding-right: 5%;
   }
 }
 br {
   @media(max-width:768px) {
     display: none;
   }
-}
-section {
-  padding: 0;
 }
 .icon {
   position: absolute;
