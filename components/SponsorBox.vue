758548b8c8896d74728ff9da57ca306be1185262
Anawin W <imonmac@hotmail.com>
2019-09-01 19:23:37 +0700

    SponsorBadge - support horizontal sponsor

--- components/SponsorBox.vue
+++ components/SponsorBox.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="logos-box">
     <div class="logos">
-      <SponsorBadge fileName="sponsors/1-1-ExxonMobil.svg" alt="ExxonMobil" link="https://corporate.exxonmobil.com/" />
       <SponsorBadge fileName="sponsors/1-2-TWA.svg" alt="Thai Webmaster Association" link="https://www.webmaster.or.th/" />
+      <SponsorBadge class="horizontal" fileName="sponsors/1-1-ExxonMobil.svg" alt="ExxonMobil" link="https://corporate.exxonmobil.com/" />
       <SponsorBadge fileName="sponsors/1-3-ITKMITL.svg" alt="IT KMITL" link="https://www.it.kmitl.ac.th/" />
     </div>
   </div>
@@ -20,7 +20,6 @@ export default {
   .logos-box {
     padding-bottom: 30px;
   }
-  
   .logos {
     display: grid;
     grid-template-columns: repeat(3, auto);
