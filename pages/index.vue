f2ce2b4bd82dcd224c32263d711945729e79b785
Anawin W <imonmac@hotmail.com>
2019-09-29 18:48:14 +0700

    index - add cta button

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <Hero />
+    <GradButton theme="pink" class="cta-button" v-scroll-to="'#major-section'">สมัครเข้าร่วมค่าย ▼</GradButton>
     <WhatIsYWC />
     <Majors />
     <Location />
@@ -15,12 +16,14 @@
 </template>
 
 <script>
+import GradButton from '~/components/GradButton'
 import WhatIsYWC from '~/components/sections/WhatIsYWC'
 import Hero from '~/components/sections/Hero'
 import Timeline from '~/components/sections/Timeline.vue'
 export default {
   components: {
     Hero,
+    GradButton,
     WhatIsYWC,
     Location: () => import('~/components/sections/Location'),
     Timeline,
@@ -40,5 +43,25 @@ export default {
   max-width: 960px;
   margin: 0 auto;
   text-align: center;
+}
+.cta-button {
+  transform: scale(1.3);
+  display: block;
+  margin: 0 auto;
+  animation: cta 0.9s alternate infinite ease-in;
+}
+@media screen and (max-width:768px) {
+  .cta-button {
+    transform: scale(1.1);
+    animation: cta-mobile 0.9s alternate infinite ease-in;
+  }
+}
+@keyframes cta {
+  0%   {transform: scale(1.3);}
+  100% {transform: scale(1.5);}
+}
+@keyframes cta-mobile {
+  0%   {transform: scale(1.1);}
+  100% {transform: scale(1.3);}
 }
 </style>
