e0452681cd85a10d2a1d05f4dd7df9ffeca76b41
Anawin W <imonmac@hotmail.com>
2019-10-21 18:34:51 +0700

    Remove unnecessary antd css

--- plugins/ant-design-vue.js
+++ plugins/ant-design-vue.js
@@ -2,11 +2,7 @@ import Vue from 'vue'
 import { Table, Button, Input, Spin, Icon, LocaleProvider } from 'ant-design-vue'
 import 'ant-design-vue/lib/table/style/index.css'
 import 'ant-design-vue/lib/empty/style/index.css'
-import 'ant-design-vue/lib/radio/style/index.css'
-import 'ant-design-vue/lib/checkbox/style/index.css'
-import 'ant-design-vue/lib/dropdown/style/index.css'
 import 'ant-design-vue/lib/spin/style/index.css'
-import 'ant-design-vue/lib/pagination/style/index.css'
 
 import 'ant-design-vue/lib/button/style/index.css'
 
