36175a1d619d044a46418637b8f5bfd6aab3917e
Anawin W <imonmac@hotmail.com>
2019-10-23 00:25:00 +0700

    Separate ThemeProvider component

--- pages/interview.vue
+++ pages/interview.vue
@@ -55,104 +55,11 @@
 </template>
 <script>
 import styled from 'vue-styled-components'
+import ThemeProvider from '~/components/ThemeProvider.vue'
 import InfoContainer from '~/components/InfoContainer.vue'
 import Footer from '~/components/sections/Footer.vue'
-import color from '~/utils/color'
+import color, { colorScheme } from '~/utils/color'
 import { majors } from '~/utils/const'
-const colorScheme = {
-  green: {
-    light: '#99F2C3',
-    normal: '#00BE7A',
-    background: 'linear-gradient(45deg, #00A030 0.08%, #00AC4F 11.17%, #00B96E 24.54%, #00BE7A 32%, #01BD93 54.11%, #01BC9D 67.51%, #01AF97 74.35%, #018D87 86.96%, #016473 100.01%)'
-  },
-  yellow: {
-    light: '#FFF6CF',
-    normal: '#FCBE4F',
-    background: 'linear-gradient(45deg, #FEE041 1.85%, #FCB46F 59.99%, #FA7398 99.64%)'
-  },
-  pink: {
-    light: '#FFCCE8',
-    normal: '#FA5C87',
-    background: 'linear-gradient(45deg, #C73884 -0.26%, #FA5C87 50.77%, #FF7745 99.73%)'
-  },
-  blue: {
-    light: '#85E5FF',
-    normal: '#4D9ED5',
-    background: 'linear-gradient(45deg, #07CACE -0.25%, #4D9ED5 50.1%, #9470DC 99.85%)'
-  }
-}
-const ThemeProvider = styled('div', { name: String })`
-  .themeText {
-    color: ${props => props.name ? colorScheme[props.name].normal : color.main};
-    font-weight: bold;
-  }
-  .themeLink, .questions a {
-    text-decoration: underline;
-    color: ${props => props.name ? colorScheme[props.name].normal : color.main};
-    &:visited, &:active, &:hover {
-       color: ${props => props.name ? colorScheme[props.name].normal : color.main};
-    }
-  }
-  .webDesign {
-    color: ${colorScheme.yellow.normal};
-  }
-
-  // ======== START Ant Design Override ========
-  // Table Head
-  .ant-table-thead > tr > th {
-    color: ${props => props.name ? colorScheme[props.name].normal : color.main};
-    border-bottom: 1px solid ${props => props.name ? colorScheme[props.name].light : colorScheme.pink.light};
-    background: none;
-    font-weight: bold;
-  }
-  // .ant-table-thead > tr > th.ant-table-column-has-actions.ant-table-column-has-sorters:hover {
-  //   background: ${props => props.name ? colorScheme[props.name].light : colorScheme.pink.light};
-  // }
-  // Table Row Hover Color
-  .ant-table-thead > tr.ant-table-row-hover:not(.ant-table-expanded-row) > td, .ant-table-tbody > tr.ant-table-row-hover:not(.ant-table-expanded-row) > td, .ant-table-thead > tr:hover:not(.ant-table-expanded-row) > td, .ant-table-tbody > tr:hover:not(.ant-table-expanded-row) > td {
-    background: ${props => props.name ? colorScheme[props.name].light : colorScheme.pink.light};
-  }
-  // Table Sorter - Inactive Color 
-  .ant-table-thead > tr > th.ant-table-column-has-actions.ant-table-column-has-sorters:active .ant-table-column-sorter-up:not(.on), .ant-table-thead > tr > th.ant-table-column-has-actions.ant-table-column-has-sorters:active .ant-table-column-sorter-down:not(.on),
-  .ant-table-thead > tr > th .ant-table-column-sorter {
-    color: ${props => props.name ? colorScheme[props.name].light : colorScheme.pink.light};
-  }
-  // Table Sorter - Active Color
-  .ant-table-thead > tr > th .ant-table-column-sorter-up.on, .ant-table-thead > tr > th .ant-table-column-sorter-down.on {
-    color: ${props => props.name ? colorScheme[props.name].normal : colorScheme.pink.normal};
-  }
-  // Table Filter Active Color 
-  .ant-table-thead > tr > th .ant-table-filter-selected.anticon-filter {
-    color: ${props => props.name ? colorScheme[props.name].normal : colorScheme.pink.normal};
-  }
-  .ant-table-filter-dropdown-link {
-    color: ${props => props.name ? colorScheme[props.name].normal : colorScheme.pink.normal};
-  }
-  // Input
-  .ant-input:hover, .ant-input:focus {
-    border-color: ${props => props.name ? colorScheme[props.name].normal : colorScheme.pink.normal};
-  }
-  .ant-input:focus {
-    box-shadow: 0 0 0 2px rgba(255, 255, 255, 0.2);
-  }
-  .ant-input-affix-wrapper:hover .ant-input:not(.ant-input-disabled) {
-    border-color: ${props => props.name ? colorScheme[props.name].normal : colorScheme.pink.normal};
-  }
-  // Spinner
-  .ant-spin-dot i {
-    background-color: ${props => props.name ? colorScheme[props.name].normal : colorScheme.pink.normal};
-  }
-  // Button 
-  .ant-btn-primary:hover, .ant-btn-primary:focus {
-    color: black;
-    background-color: ${props => props.name ? colorScheme[props.name].light : colorScheme.pink.light};
-    border-color: ${props => props.name ? colorScheme[props.name].light : colorScheme.pink.light};
-  }
-  .ant-btn-primary, .ant-btn-primary:active, .ant-btn-primary.active {
-    background-color: ${props => props.name ? colorScheme[props.name].normal : colorScheme.pink.normal};
-    border-color: ${props => props.name ? colorScheme[props.name].normal : colorScheme.pink.normal};
-  }
-`
 const MajorRow = styled('div')`
   display: grid;
   max-width: 1056px;
