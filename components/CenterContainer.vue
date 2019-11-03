aa994c2eafccdab673b29ea086dfbdc59888175f
Anawin W <imonmac@hotmail.com>
2019-11-04 02:56:37 +0700

    Add new components and assets

---
+++ components/CenterContainer.vue
@@ -0,0 +1,31 @@
+<script>
+import styled from 'vue-styled-components'
+
+const CenterContainer = styled.div`
+  max-width: 960px;
+  width: 100%;
+  min-height: 60%;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background: #ffffff;
+  box-shadow: 0px 15px 35px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & > div {
+    text-align: center;
+  }
+  @media screen and (max-width: 576px) {
+    position: static;
+    left: unset;
+    top: unset;
+    transform: unset;
+    margin-top: 40px;
+    margin-bottom: 44px;
+  }
+`
+export default CenterContainer
+</script>
---
+++ components/result/Button.vue
@@ -0,0 +1,44 @@
+<template>
+  <ButtonLink v-if="link">
+    <slot />
+  </ButtonLink>
+  <Button v-else @click="$emit('click')">
+    <slot />
+  </Button>
+</template>
+<script>
+import styled from 'vue-styled-components'
+const ButtonStyle = `
+display: inline-block;
+width: fit-content;
+background: #E1426F;
+box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.15);
+border-radius: 100px;
+border: none;
+outline: none;
+
+cursor: pointer;
+
+font-family: Maledpan;
+font-weight: bold;
+font-size: 20px;
+text-align: center;
+padding: 7px 20px;
+color: white;
+`
+const Button = styled(`button`)`
+${ButtonStyle}
+`
+const ButtonLink = styled(`a`)`
+${ButtonStyle}
+`
+export default {
+  components: {
+    Button,
+    ButtonLink
+  },
+  props: {
+    link: { type: Boolean, default: false }
+  }
+}
+</script>
