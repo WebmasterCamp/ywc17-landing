c7a3e24858cf7a2e72a3b1c75956f73e645e6ab5
bazsup <supawit.ruen@mail.kmutt.ac.th>
2019-12-25 16:25:50 +0700

    :pencil2: Update agenda web content

--- utils/const.js
+++ utils/const.js
@@ -152,7 +152,7 @@ export const AGENDA = [
       { time: '8:00 - 8:30', events: ['เดินทางสู่สถานที่จัดกิจกรรม'] },
       { time: '8:30 - 10:00', events: ['กิจกรรมสันทนาการ'] },
       { time: '10:00 - 10:15', events: ['พักรับประทานอาหารว่าง'] },
-      { time: '10:15 - 11:15', events: ['การบรรยายแยกสาขา', 'สาขา Web Marketing หัวข้อ <br>“เครื่องมือโฆษณาสำหรับนักการตลาดดิจิทัล”<br>โดย คุณเมธปรียา คํานวนวุฒิ, <i>ผู้จัดการฝ่ายการตลาด LnwShop Co., Ltd.</i>', 'สาขา Web Design หัวข้อ <br>“User Experience for Web Design”<br>โดย คุณสุธัม ธรรมวงศ์, <i>Senior UX Designer at SCB10X</i>', 'สาขา Web Content (อยู่ระหว่างประสานงาน)', 'สาขา Web Programming หัวข้อ<br>“How to be Great Software Developer”<br>โดย คุณปัญจมพงศ์ เสริมสวัสดิ์ศรี, <i>C-3PO at Cleverse</i>'] },
+      { time: '10:15 - 11:15', events: ['การบรรยายแยกสาขา', 'สาขา Web Marketing หัวข้อ <br>“เครื่องมือโฆษณาสำหรับนักการตลาดดิจิทัล”<br>โดย คุณเมธปรียา คํานวนวุฒิ, <i>ผู้จัดการฝ่ายการตลาด LnwShop Co., Ltd.</i>', 'สาขา Web Design หัวข้อ <br>“User Experience for Web Design”<br>โดย คุณสุธัม ธรรมวงศ์, <i>Senior UX Designer at SCB10X</i>', 'สาขา Web Content หัวข้อ<br>“การสร้างและจัดการ Community”<br>โดย คุณอภิศิลป์ ตรุงกานนท์, Chief Product Officer @ Pantip', 'สาขา Web Programming หัวข้อ<br>“How to be Great Software Developer”<br>โดย คุณปัญจมพงศ์ เสริมสวัสดิ์ศรี, <i>C-3PO at Cleverse</i>'] },
       { time: '11:15 - 12:30', events: ['ระดมความคิด (Brainstorm) 3', 'รับประทานอาหารกลางวัน'] },
       { time: '12:30 - 16:30', events: ['ลงพื้นที่เก็บข้อมูลจริงจากผู้ใช้'] },
       { time: '16:30 - 18:00', events: ['การบรรยาย “วิธีการนำเสนอผลงานอย่างมืออาชีพ”<br>โดย คุณผรินทร์ สงฆ์ประชา, <i>Chief Executive and Founder of Nasket</i>'] },
